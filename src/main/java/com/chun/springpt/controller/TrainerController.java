1a9a8a780813a820b6d1cff546c4bcd7de578259
nias2734 <nias2734@naver.com>
2024-01-05 15:40:41 +0900

    트레이너 컨트롤러 수정
    20240105_이성한

--- src/main/java/com/chun/springpt/controller/TrainerController.java
+++ src/main/java/com/chun/springpt/controller/TrainerController.java
@@ -7,14 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chun.springpt.service.TrainerListService;
 import com.chun.springpt.vo.TrainerVO;
 
 @RestController
 public class TrainerController {
+	
+	@Autowired
+	private TrainerListService Tservice;
+	
     @GetMapping("/trainerList")
     public List<TrainerVO> getTrainerList(){
-        List<TrainerVO> list =  new ArrayList<>();
-        
+        return Tservice.selecTrainerList();
+    	
+    	//List<TrainerVO> list = new ArrayList<>(); 기존코드   
     }
-    
 }
