9700e76a0ae3a29aba93790f3b0dbcbe93ae3a79
chunsik <sb97213@gmail.com>
2024-01-10 21:25:17 +0900

    토큰

--- src/main/java/com/chun/springpt/controller/AuthController.java
+++ src/main/java/com/chun/springpt/controller/AuthController.java
@@ -2,23 +2,41 @@ package com.chun.springpt.controller;
 
 import com.chun.springpt.domain.dto.LoginRequest;
 import com.chun.springpt.service.AuthService;
+import com.chun.springpt.utils.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class AuthController {
 
+    @Autowired
+    private HttpServletRequest request;
+
     private final AuthService authService;
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginRequest dto) {
-        log.info("넘어온 아이디: {}",dto.getUserName());
-        log.info("넘어온 비밀번호: {}",dto.getPassword());
+//        log.info("넘어온 아이디: {}",dto.getUserName());
+//        log.info("넘어온 비밀번호: {}",dto.getPassword());
         return ResponseEntity.ok().body(authService.login(dto.getUserName(), dto.getPassword()));
     }
+
+    @GetMapping("/checkToken")
+    public ResponseEntity<?> checkToken() {
+        // 헤더에서 토큰 추출
+        String authorizationHeader = request.getHeader("Authorization");
+        String token = JwtUtil.extractToken(authorizationHeader);
+
+        // 사용자 권한
+        String userRole = JwtUtil.getRole(token);
+
+        return ResponseEntity.ok().body(userRole);
+    }
+
+
 }
--- src/main/java/com/chun/springpt/controller/TokenTestController.java
+++ src/main/java/com/chun/springpt/controller/TokenTestController.java
@@ -4,29 +4,35 @@ import com.chun.springpt.utils.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LoginTestController {
+public class TokenTestController {
 
     @Autowired
     private HttpServletRequest request;
 
+    // 토큰을 핸들링하는 컨트롤러
     @PostMapping("/logintest")
-    public ResponseEntity<String> loginTest(Authentication authentication) {
+    public ResponseEntity<String> loginTest() {
 
         // 헤더에서 토큰 추출
         String authorizationHeader = request.getHeader("Authorization");
         String token = JwtUtil.extractToken(authorizationHeader);
 
         // 사용자 아이디
-        String userName = authentication.getName();
+        String userName = JwtUtil.getID(token);
 
         // 사용자 권한
         String userRole = JwtUtil.getRole(token);
 
         return ResponseEntity.ok().body("유저 아이디 : " + userName + " 유저 권한 : " + userRole);
+    }
+
+    // 토큰의 유효성 검사만 하고 토큰을 핸들링 하지는 않음
+    @PostMapping("/logintest2")
+    public ResponseEntity<?> loginTest2() {
+        return ResponseEntity.ok().body("유효한 토큰 잘 들어와서 통신 성공");
     }
 }
