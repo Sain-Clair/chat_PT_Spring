1508c60483aa9c62671d4b804b6809956063f84b
chunsikman <sb97213@naver.com>
2024-01-21 07:21:26 +0900

    장고와 연결 테스트

--- src/main/java/com/chun/springpt/controller/DietController.java
+++ src/main/java/com/chun/springpt/controller/DietController.java
@@ -1,6 +1,7 @@
 package com.chun.springpt.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -31,6 +32,9 @@ public class DietController {
     @Autowired
     private DietService Dservice;
 
+    @Value("${django.base.url}")
+    private String djangoBaseUrl;
+
     @GetMapping("/food_recommand")
     public Map<String, Object> foodRecommand() {
         // 헤더에서 토큰 추출
@@ -42,7 +46,7 @@ public class DietController {
         log.info("로깅 - 요청아이디: {}", userName);
 
         // 외부 URL로부터 데이터 가져오기
-        String url = "http://localhost:9000/dlmodel/getCal?id=" + userName;
+        String url = djangoBaseUrl + "/dlmodel/getCal?id=" + userName;
         RestTemplate restTemplate = new RestTemplate();
         Map<String, Object> response = restTemplate.getForObject(url, Map.class);
 
