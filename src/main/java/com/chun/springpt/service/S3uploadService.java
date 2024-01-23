3f2283091ec6974177c91defb3dc2498ff69474b
ysh <yoonsh1028@naver.com>
2024-01-23 09:10:54 +0900

    사용안한 메소드 삭제

--- src/main/java/com/chun/springpt/service/S3uploadService.java
+++ src/main/java/com/chun/springpt/service/S3uploadService.java
@@ -36,23 +36,4 @@ public class S3uploadService {
         }
         return fileUrls;
     }
-
-    public List<String> normal_mem(String path, List<MultipartFile> uploadImgs) throws IOException {
-        List<String> fileUrls = new ArrayList<>();
-
-        for (MultipartFile multipartFile : uploadImgs) {
-            String originalFilename = multipartFile.getOriginalFilename();
-
-            String fullPath = path + originalFilename;
-
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(multipartFile.getSize());
-            metadata.setContentType(multipartFile.getContentType());
-
-            amazonS3.putObject(bucket, fullPath, multipartFile.getInputStream(), metadata);
-            fileUrls.add(amazonS3.getUrl(bucket, fullPath).toString());
-        }
-        return fileUrls;
-    }
-
 }
