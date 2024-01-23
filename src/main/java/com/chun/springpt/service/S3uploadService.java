218ab4402eba41a25bef6973702472aa75ce9c45
momorichan <wonkj111@naver.com>
2024-01-23 11:49:06 +0900

    음식 이름 수정 요청 쿼리 추가 및 수정

--- src/main/java/com/chun/springpt/service/S3uploadService.java
+++ src/main/java/com/chun/springpt/service/S3uploadService.java
@@ -1,13 +1,17 @@
 package com.chun.springpt.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,5 +40,28 @@ public class S3uploadService {
         }
 
         return fileUrls;
+    }
+
+
+    public String saveFilewithName(String path, byte[] imageBytes) throws IOException {
+        String fileUrls = "";
+
+        if (imageBytes != null && imageBytes.length > 0) {
+            InputStream inputStream = new ByteArrayInputStream(imageBytes);
+
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(imageBytes.length);
+            metadata.setContentType("image/jpeg"); // 이 부분은 필요에 따라 조정 가능
+
+            // S3에 파일 업로드
+            amazonS3.putObject(new PutObjectRequest(bucket, path, inputStream, metadata));
+            fileUrls = amazonS3.getUrl(bucket, path).toString();
+        }
+
+        return fileUrls;
+    }
+
+    public void deleteFileFromS3(String filePath) {
+        amazonS3.deleteObject(new DeleteObjectRequest(bucket, filePath));
     }
 }
